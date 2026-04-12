3936bad44a6ba23d9da4063832aa4cb64a19abbd
rehnazraffiq <rm4109@srmist.edu.in>
2026-04-12 14:50:19 +0530

    [rehnazraffiq] Added UC16:Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,39 +1,29 @@
 import java.util.*;
-import java.util.stream.Stream;
-
 public class TrainConsistManagementApp{
-    static class GoodsBogie{
-        String type;
-        String cargo;
-
-        GoodsBogie(String type,String cargo){
-            this.type = type;
-            this.cargo = cargo;
-        }
-    }
     public static void main(String[] args){
-        System.out.println("==================================================");
-        System.out.println("UC12 - Safety Compliance Check for Goods Bogies");
-        System.out.println("==================================================");
-        System.out.println();
-        System.out.println("Goods Bogies in Train: ");
-        GoodsBogie g1 = new GoodsBogie("Cylindrical","Petroleum");
-        GoodsBogie g2 = new GoodsBogie("Open","Coal");
-        GoodsBogie g3 = new GoodsBogie("Box","Grain");
-        GoodsBogie g4 = new GoodsBogie("Cylindrical","Coal");
-        List<GoodsBogie> goodsBogies = new ArrayList<>();
-        goodsBogies.add(g1);
-        goodsBogies.add(g2);
-        goodsBogies.add(g3);
-        goodsBogies.add(g4);
-        for(GoodsBogie goodBogie : goodsBogies){
-            System.out.println(goodBogie.type+ " -> " +goodBogie.cargo);
+        System.out.println("==========================================");
+        System.out.println("UC16 - Manual Sorting using Bubble Sort");
+        System.out.println("==========================================");
+
+        int[] capacities = {72,56,24,70,60};
+
+        System.out.println("\nOriginal Capacities: ");
+        for (int c : capacities){
+            System.out.print(c+ " ");
         }
-        System.out.println();
-        boolean result = goodsBogies.stream().allMatch(b -> Objects.equals(b.cargo, "Petroleum"));
-        System.out.println("Safety Compliance Status: " +result);
-        System.out.println("Train formation is NOT SAFE.");
-        System.out.println();
-        System.out.println("UC12 safety validation completed...");
+        for(int i = 0; i < capacities.length - 1; i++){
+            for(int j = 0; j < capacities.length - 1 - i; j++){
+                if(capacities[j] > capacities[j+1]){
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j+1];
+                    capacities[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("\n\nSorted Capacities (Ascending):");
+        for (int c : capacities){
+            System.out.print(c+ " ");
+        }
+        System.out.println("\n\nUC16 sorting completed...");
     }
 }
