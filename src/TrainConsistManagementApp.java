d177e340abae156982a5835c4d39c7e6a9de454c
rehnazraffiq <rm4109@srmist.edu.in>
2026-04-12 16:22:06 +0530

    [rehnazraffiq] Added UC17:Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,41 +1,20 @@
-import java.util.*;
-
+import java.util.Arrays;
 public class TrainConsistManagementApp{
-    static class CargoSafetyException extends RuntimeException{
-        public CargoSafetyException(String message){
-            super(message);
-        }
-    }
-    static class GoodsBogie{
-        String shape;
-        String cargo;
-
-        GoodsBogie(String shape){
-            this.shape = shape;
-        }
-        void assignCargo(String cargo){
-            try{
-                if (cargo.equals("Petroleum") && shape.equals("Rectangular")){
-                    throw new CargoSafetyException("Unsafe cargo assignment");
-                }
-                System.out.println("\nCargo assigned successfully -> " +cargo);
-            }catch (CargoSafetyException e){
-                System.out.println("\nError: " +e.getMessage());
-            }finally {
-                System.out.println("Cargo validation completed for " +shape+ " bogie");
-            }
-        }
-    }
     public static void main(String[] args){
-        System.out.println("=========================================");
-        System.out.println("UC15 - Safe Cargo Assignment ");
-        System.out.println("=========================================");
+        System.out.println("==============================================");
+        System.out.println("UC17 - Sort Bogie Names Using Arrays.sort()");
+        System.out.println("==============================================");
 
-        GoodsBogie g1 = new GoodsBogie("Cylindrical");
-        g1.assignCargo("Petroleum");
-        GoodsBogie g2 = new GoodsBogie("Rectangular");
-        g2.assignCargo("Petroleum");
+        String[] bogieNames = {"Sleeper" , "AC Chair" , "First Class" , "General" , "Luxury"};
 
-        System.out.println("\nUC15 runtime handling completed...");
+        System.out.println("\nOriginal Bogie Names: ");
+        System.out.print(Arrays.toString(bogieNames));
+
+        Arrays.sort(bogieNames);
+
+        System.out.println("\n\nSorted Bogie Names (Alphabetical): ");
+        System.out.print(Arrays.toString(bogieNames));
+
+        System.out.println("\n\nUC17 sorting completed...");
     }
 }
